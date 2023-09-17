5c34e6222d132e79b8c483c72a134c27120148ff
Szarath Kumar <szarath@users.noreply.github.com>
2023-09-17 20:51:14 +0200

    Seperated Class

---
+++ Payment_Gateway/PaymentGateway.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payment_Gateway
+{
+    public class PaymentGateway
+    {
+        private Dictionary<string, decimal> accounts = new Dictionary<string, decimal>();
+
+        public void CreateAccount(string accountId, decimal balance = 0)
+        {
+            if (accounts.ContainsKey(accountId))
+            {
+                throw new Exception("Account already exists");
+            }
+            accounts[accountId] = balance;
+        }
+
+        public decimal CheckBalance(string accountId)
+        {
+            if (!accounts.ContainsKey(accountId))
+            {
+                throw new Exception("Account not found");
+            }
+            return accounts[accountId];
+        }
+
+        public bool ProcessPayment(string senderAccountId, string recipientAccountId, decimal amount)
+        {
+            if (!accounts.ContainsKey(senderAccountId))
+            {
+                throw new Exception("Sender account not found");
+            }
+            if (!accounts.ContainsKey(recipientAccountId))
+            {
+                throw new Exception("Recipient account not found");
+            }
+            if (amount <= 0)
+            {
+                throw new Exception("Invalid amount");
+            }
+
+            decimal senderBalance = accounts[senderAccountId];
+            if (amount > senderBalance)
+            {
+                throw new Exception("Insufficient funds");
+            }
+
+            // Simulate deducting the amount from the sender
+            accounts[senderAccountId] -= amount;
+
+            // Simulate adding the amount to the recipient
+            accounts[recipientAccountId] += amount;
+
+            // Simulate a successful transaction
+            return true;
+        }
+    }
+}
--- Payment_Gateway/Program.cs
+++ Payment_Gateway/Program.cs
@@ -1,59 +1,6 @@
-﻿using System;
+﻿using Payment_Gateway;
+using System;
 using System.Collections.Generic;
-
-public class PaymentGateway
-{
-    private Dictionary<string, decimal> accounts = new Dictionary<string, decimal>();
-
-    public void CreateAccount(string accountId, decimal balance = 0)
-    {
-        if (accounts.ContainsKey(accountId))
-        {
-            throw new Exception("Account already exists");
-        }
-        accounts[accountId] = balance;
-    }
-
-    public decimal CheckBalance(string accountId)
-    {
-        if (!accounts.ContainsKey(accountId))
-        {
-            throw new Exception("Account not found");
-        }
-        return accounts[accountId];
-    }
-
-    public bool ProcessPayment(string senderAccountId, string recipientAccountId, decimal amount)
-    {
-        if (!accounts.ContainsKey(senderAccountId))
-        {
-            throw new Exception("Sender account not found");
-        }
-        if (!accounts.ContainsKey(recipientAccountId))
-        {
-            throw new Exception("Recipient account not found");
-        }
-        if (amount <= 0)
-        {
-            throw new Exception("Invalid amount");
-        }
-
-        decimal senderBalance = accounts[senderAccountId];
-        if (amount > senderBalance)
-        {
-            throw new Exception("Insufficient funds");
-        }
-
-        // Simulate deducting the amount from the sender
-        accounts[senderAccountId] -= amount;
-
-        // Simulate adding the amount to the recipient
-        accounts[recipientAccountId] += amount;
-
-        // Simulate a successful transaction
-        return true;
-    }
-}
 
 class Program
 {
